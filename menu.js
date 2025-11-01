4e8724617366be969c0cdd738cf80a753f8bf73d
Issouf-k <issouf123h@gmail.com>
2025-11-01 13:49:27 +0000

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,110 @@
+export function generateMenu(pseudo, prefix) {
+  return `
+⛧━━━━━━━━━━━━━━━━━━━⛧
+      👁️ 𝕶𝖆𝖓𝖊𝖐𝖎 𝕲𝖍𝖔𝖚𝖑 🩸  
+      𝕍𝕒𝕟𝕤🅑🅞🅣 🩸 𝕍4.2  
+*"Je ne suis ni humain… ni ghoul."*
+⛧━━━━━━━━━━━━━━━━━━━⛧
+
+🕷️ 𝕮𝖆𝖕𝖎𝖙𝖆𝖎𝖓𝖊 : 𝖀𝖓𝖊-𝖊𝖞𝖊𝖉 𝕲𝖍𝖔𝖚𝖑  
+👤 𝖀𝖙𝖎𝖑𝖎𝖘𝖆𝖙𝖊𝖚𝖗 : ${pseudo}  
+☕ 𝕮𝖆𝖋𝖊́ 𝕬𝖓𝖙𝖊𝖎𝖐𝖚 — 𝖅𝖔𝖓𝖊 𝖘𝖊́𝖈𝖚𝖗𝖎𝖘𝖊́𝖊  
+⛧━━━━━━━━━━━━━━━━━━━━⛧
+
+🩸 𝕮𝖆𝖙𝖊́𝖌𝖔𝖗𝖎𝖊 : 𝕲𝖊́𝖓𝖊́𝖗𝖆𝖑 👁️
+⛧━━━━━━━━━━━━━━⛧
+  ⚡ ${prefix}ping
+  📅 ${prefix}date
+  ☎️ ${prefix}contact
+  🗺️ ${prefix}loc
+  🔍 ${prefix}info
+  🖼️ ${prefix}setpp
+  🖼️ ${prefix}getpp
+  🧠 ${prefix}quote
+  🎭 ${prefix}autoreact
+  ✍️ ${prefix}autowrite
+  📝 ${prefix}vcfgroup
+  👁️ ${prefix}autovustatut
+  🎨 ${prefix}take
+  🔄 ${prefix}setprefixe
+⛧━━━━━━━━━━━━━━⛧
+
+🩸 𝕮𝖆𝖙𝖊́𝖌𝖔𝖗𝖎𝖊 : 𝕸𝖊́𝖉𝖎𝖆𝖘 👁️
+⛧━━━━━━━━━━━━━━⛧
+  🖼️ ${prefix}img
+  🎞️ ${prefix}gif
+  🎬 ${prefix}vid
+  🔊 ${prefix}aud
+  ✨ ${prefix}stk
+  🖌️ ${prefix}tostk
+  👁️ ${prefix}vu / vv
+  🎵 ${prefix}toaud
+  💥 ${prefix}react
+⛧━━━━━━━━━━━━━━⛧
+
+🩸 𝕮𝖆𝖙𝖊́𝖌𝖔𝖗𝖎𝖊 : 𝕾𝖙𝖔𝖈𝖐𝖆𝖌𝖊 👁️
+⛧━━━━━━━━━━━━━━⛧
+  💾 ${prefix}store
+  📋 ${prefix}list
+  🗑️ ${prefix}delmedia
+⛧━━━━━━━━━━━━━━⛧
+
+🩸 𝕮𝖆𝖙𝖊́𝖌𝖔𝖗𝖎𝖊 : 𝕬𝖉𝖒𝖎𝖓𝖎𝖘𝖙𝖗𝖆𝖙𝖎𝖔𝖓 👁️
+⛧━━━━━━━━━━━━━━⛧
+  🎉 ${prefix}welcome
+  ⬆️ ${prefix}promote
+  ⬇️ ${prefix}demote
+  🚫 ${prefix}kick / ban
+  🚫 ${prefix}kickall / banall (oneshot)
+  🔇 ${prefix}mute
+  🔈 ${prefix}unmute
+  📢 ${prefix}tagadmin
+  📢 ${prefix}tagall
+  🕵️ ${prefix}groupinfo
+  🔗 ${prefix}antilink
+  🛡️ ${prefix}antimedia
+  🤖 ${prefix}antibot
+  ⚠️ ${prefix}antispam
+  ⚠️ ${prefix}antitag
+  ❌ ${prefix}antipromote
+  ❌ ${prefix}antidemote
+  🩸 ${prefix}antistatut 
+  🚫 ${prefix}antitransfer
+  🧩 ${prefix}forbiddenword
+  📊 ${prefix}levelsystem
+  💬 ${prefix}responder
+  👋 ${prefix}goodbye
+⛧━━━━━━━━━━━━━━⛧
+
+🩸 𝕮𝖆𝖙𝖊́𝖌𝖔𝖗𝖎𝖊 : 𝕯𝖎𝖛𝖊𝖗𝖙𝖎𝖘𝖘𝖊𝖒𝖊𝖓𝖙 👁️
+⛧━━━━━━━━━━━━━━⛧
+  👨‍💼 ${prefix}mindset
+  👑 ${prefix}motivation
+  🎵 ${prefix}playaudio
+  🎬 ${prefix}playvideo
+  📺 ${prefix}youtube / yt
+  📱 ${prefix}tiktok / tk
+  🎲 ${prefix}quiz
+⛧━━━━━━━━━━━━━━⛧
+
+🩸 𝕮𝖆𝖙𝖊́𝖌𝖔𝖗𝖎𝖊 : 𝕬𝖎𝖉𝖊 👁️
+⛧━━━━━━━━━━━━━━⛧
+  ❓ ${prefix}help / menu
+  📚 ${prefix}doc
+  🔄 ${prefix}updates
+  ℹ️ ${prefix}about
+⛧━━━━━━━━━━━━━━⛧
+
+🩸━━━━━━━━━━━━━━━🩸
+𝕯𝖊́𝖛𝖊𝖑𝖔𝖕𝖕𝖊́ 𝖕𝖆𝖗 : VANSCODE 
+*"Souviens-toi… même les monstres ont une âme."*  
+🩸━━━━━━━━━━━━━━━🩸
+`;
+}
+/* tous droits réservés © VANSCODE 2024 */
+/* vous pouvez modifier le code je vous y autorise , à condition de respecter les droite d'auteur : 
+vous n'avez pas le droit de dire ' creer par ....' mais plutot 'modifier de vansbot par...'
+sous peine de banissement possible du compte si constater par l'auteur,
+ainsi que des poursuites judiciaires.
+Merci de votre compréhension et bonne continuation dans vos projets. 
+*/
